0c11d1fe940b358a7dbfc5363cdab3e61322d0f6
Valpeters Marina <mvalpeters@gmail.com>
2022-08-10 10:00:20 +0300

    zadacha_13

---
+++ zadacha_13/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+string GetThirdNumber(int number3)
+{
+    int res1 = number3 % 10;
+    string res = Convert.ToString(res1);
+    Console.WriteLine(number3/100);
+    if ((number3/100 > 0)&(number3/100 <= 9))
+
+  {
+    res1 = number3 % 10;
+    res = Convert.ToString(res1);
+
+  }
+  else {res = "третьей цифры нет";}
+    return res;
+}
+int number3 = new Random().Next(1,10000);
+string second = GetThirdNumber(number3);
+Console.WriteLine($"Из числа {number3} получили: {second}");
