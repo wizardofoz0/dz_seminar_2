57f7ea0b1be1a152407d9ed6db0628d8817af188
Valpeters Marina <mvalpeters@gmail.com>
2022-08-10 10:51:01 +0300

    zadacha_13 update

--- zadacha_13/Program.cs
+++ zadacha_13/Program.cs
@@ -11,17 +11,26 @@ string GetThirdNumber(int number3)
 {
     int res1 = number3 % 10;
     string res = Convert.ToString(res1);
-    Console.WriteLine(number3/100);
-    if ((number3/100 > 0)&(number3/100 <= 9))
-
+    if (number3/100 > 0) 
   {
-    res1 = number3 % 10;
-    res = Convert.ToString(res1);
+    res1=number3;
+    Console.WriteLine(res1);
 
+    while (res1>1000)
+    {
+        res1=res1/10;
+        Console.WriteLine(res1);
+    }
+    res1 = res1 % 10;
+    res = Convert.ToString(res1);
   }
-  else {res = "третьей цифры нет";}
+  else
+  {
+    res = "третьей цифры нет";
+    }
     return res;
+
 }
-int number3 = new Random().Next(1,10000);
+int number3 = new Random().Next(10000,99999);
 string second = GetThirdNumber(number3);
 Console.WriteLine($"Из числа {number3} получили: {second}");
